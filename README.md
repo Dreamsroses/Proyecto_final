17390af93b218b071f616e433b262efe033b3875
Rosa <rosa.morales.rojas@gmail.com>
2018-08-30 21:26:39 -0300

    se agrega theme php

--- README.md
+++
@@ -1,3 +0,0 @@
-Proyecto Final Rosa Morales R.
-
-Inicio: 17 de Agosto 2018
--- functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require get_template_directory() . '/functions/bootstrap-navwalker.php';
-
-register_nav_menus( array(
-    'menu-1' => esc_html__( 'Primary', 'theme-textdomain' ),
-) );
